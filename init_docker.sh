8d98c07b31a5b9e26a2aab8b9814636ee0501733
ahoone <nvbs.software@gmail.com>
2026-02-05 15:01:43 +0100

    docker script

--- init_docker.sh
+++ init_docker.sh
@@ -6,11 +6,11 @@ set -euo pipefail  # Exit on errors, undefined variables
 # https://docs.docker.com/engine/install/debian/
 #######################################
 # Uninstall all conflicting packages:
-sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+sudo apt remove -y $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1) || true
 
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -27,7 +27,7 @@ EOF
 sudo apt update
 
 # Install the latest version:
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #######################################
 
 # Making docker usable without sudo
